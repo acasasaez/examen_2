a8c5e98d9a94ed3f3eb03c5efd1f5a08dfe54d02
acasasaez <acasasae@myuax.com>
2022-04-08 13:08:32 +0200

    Update pokemon_tierra.py

--- pokemon/pokemon_tierra.py
+++ pokemon/pokemon_tierra.py
@@ -1,4 +1,9 @@
-class PokemonEarth():
+from pokemon.pokemon_clase import Pokemon
+
+
+class PokemonEarth(Pokemon):
+    def __init__():
+        super().__init__()
     """Python class to implement a basic version of a Pokemon of the game.
 
     This Python class implements the basic version of a Pokemon of the game.
