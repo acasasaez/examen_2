25e750a081ec8a0517bb900cc4719d34cd531085
acasasaez <acasasae@myuax.com>
2022-04-08 12:01:01 +0200

    ,

---
+++ pokemon_clase.py
@@ -0,0 +1,172 @@
+class Pokemon():
+    """Python class to implement a basic version of a Pokemon of the game.
+
+    This Python class implements the basic version of a Pokemon of the game.
+
+    Syntax
+    ------
+      obj = Pokemon(id, pokemon_name, weapon_type, health_points,
+                   attack_rating, defense_rating)
+
+    Parameters
+    ----------
+      [in] id ID of the Pokemon.
+      [in] pokemon_name Name of the Pokemon.
+      [in] weapon_type Type of weapon that carries out the Pokemon.
+      [in] health_points Points of health that the Pokemon has.
+      [in] attack_rating Attack rating of the Pokemon.
+      [in] defense_rating Defense rating of the Pokemon.
+
+    Returns
+    -------
+      obj Python object output parameter that represents an instance
+          of the class Pokemon.
+
+    Attributes
+    ----------
+
+    Example
+    -------
+      >>> from pokemon import Pokemon
+      >>> from weapon_type import WeaponType
+      >>> obj_Pokemon = Pokemon(1, "Bulbasaur", WeaponType.PUNCH, 100, 7, 10)
+    """
+    def __init__(self, id, pokemon_name, weapon_type, health_point, attack_rating,  defense_rating):
+        self.id = id 
+        self.pokemon_name = pokemon_name
+        self.weapon_type = weapon_type
+        self.health_point = health_point
+        self.attack_rating = attack_rating
+        self.defense_rating = defense_rating
+
+def get_id (self):
+    return self.id
+def set_id (self,id):
+    self.id = id
+#Repetir con todos los parámetros del constructor 
+#Guardar en archivo .csv  que nos lo guardará en una tabla tipo excel 
+
+
+
+
+def main():
+    """Function main of the module.
+
+    The function main of this module is used to test the Class that is described
+    in this module.
+
+    Syntax
+    ------
+      [ ] = main()
+
+    Parameters
+    ----------
+      Null .
+
+    Returns
+    -------
+      Null .
+
+    Example
+    -------
+      >>> main()
+    """
+
+    print("=================================================================.")
+    print("Test Case 1: Create a Pokemon.")
+    print("=================================================================.")
+    pokemon_1 = Pokemon(1, "Ivysaur", WeaponType.HEADBUTT, 100, 8, 9)
+
+    if pokemon_1.get_pokemon_name() == "Ivysaur":
+        print("Test PASS. The parameter pokemon_name has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_weapon_type().name == "HEADBUTT":
+        print("Test PASS. The parameter weapon_type has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_health_points() == 100:
+        print("Test PASS. The parameter health_points has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_attack_rating() == 8:
+        print("Test PASS. The parameter attack_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+    if pokemon_1.get_defense_rating() == 9:
+        print("Test PASS. The parameter defense_rating has been correctly set.")
+    else:
+        print("Test FAIL. Check the method __init__().")
+
+
+    print("=================================================================.")
+    print("Test Case 2: Human-readable format of the object.")
+    print("=================================================================.")
+    pokemon_2 = Pokemon(2, "Charmander", WeaponType.HEADBUTT, 100, 7, 10)
+
+    if str(pokemon_2) == "Pokemon ID 2 with name Charmander has as weapon HEADBUTT and health 100":
+        print("Test PASS. The human-readable format of the object has been implemented correctly.")
+    else:
+        print("Test FAIL. Check the method __str__()." + " RESULT: " + str(pokemon_2))
+
+
+    print("=================================================================.")
+    print("Test Case 3: Pokemon alive?¿?.")
+    print("=================================================================.")
+    pokemon_3 = Pokemon(3, "Wartortle", WeaponType.KICK, 97, 8, 9)
+
+    if pokemon_3.is_alive():
+        pokemon_3.fight_defense(200)  # With this the Pokemon should be retired.
+
+        if not pokemon_3.is_alive():
+            print("Test PASS. The method is_alive() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method is_alive().")
+    else:
+        print("Test FAIL. Check the method is_alive().")
+
+
+    print("=================================================================.")
+    print("Test Case 4: Check the defense during a Fight.")
+    print("=================================================================.")
+    pokemon_4 = Pokemon(4, "Squirtle", WeaponType.ELBOW, 93, 9, 6)
+
+    pokemon_4.fight_defense(70)
+
+    if pokemon_4.get_health_points() == 29:
+        print("Test PASS. The method fight_defense() has been implemented correctly.")
+    else:
+        print("Test FAIL. Check the method fight_defense().")
+
+
+    print("=================================================================.")
+    print("Test Case 5: Check the attack during a Fight.")
+    print("=================================================================.")
+    pokemon_5 = Pokemon(5, "Venusaur", WeaponType.PUNCH, 99, 10, 7)
+    pokemon_6 = Pokemon(6, "Charmeleon", WeaponType.PUNCH, 99, 9, 8)
+
+    pokemon_was_hit = pokemon_5.fight_attack(pokemon_6)
+
+    if pokemon_was_hit:
+        if pokemon_6.get_health_points() == 97:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+    else:
+        if pokemon_6.get_health_points() == 99:
+            print("Test PASS. The method fight_attack() has been implemented correctly.")
+        else:
+            print("Test FAIL. Check the method fight_attack().")
+
+
+
+# Checking whether this module is executed just itself alone.
+if __name__ == "__main__":
+    main()
+
+
+# EOF
